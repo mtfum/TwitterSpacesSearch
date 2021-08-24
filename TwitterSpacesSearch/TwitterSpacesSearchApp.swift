a1842beb86adf1697352529d0c510d1d7bd989dc
mtfum <f.yamanaka1127@gmail.com>
2021-08-24 15:03:51 -0700

    Initial Commit

---
+++ TwitterSpacesSearch/TwitterSpacesSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSpacesSearchApp.swift
+//  TwitterSpacesSearch
+//
+//  Created by Fumiya Yamanaka on 2021/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSpacesSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
