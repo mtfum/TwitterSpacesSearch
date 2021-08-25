15aa23f79c5d84bb70be91c9428cbce3a1f81e6f
mtfum <f.yamanaka1127@gmail.com>
2021-08-24 19:17:32 -0700

    :building_construction: Introduce SwiftPM

---
+++ Sources/TwitterSpacesSearch/TwitterSpacesSearch.swift
@@ -0,0 +1,6 @@
+public struct TwitterSpacesSearch {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/TwitterSpacesSearchTests/TwitterSpacesSearchTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import TwitterSpacesSearch
+
+final class TwitterSpacesSearchTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(TwitterSpacesSearch().text, "Hello, World!")
+    }
+}
